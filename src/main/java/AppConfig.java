b5e40d5657b8c67f898f33ee592db8a9614fd4c4
VIT363 <mexx-363@yandex.ru>
2023-06-22 16:49:12 +0300

    2.1.3

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -2,13 +2,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
-@Configuration
+@Configuration //конфигурационный класс
 public class AppConfig {
- 
+
     @Bean(name="helloworld")
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
+    }
+
+    @Bean(name = "cat")
+    @Scope("prototype")
+    public Cat cat_Pojo() {
+        return new Cat();
     }
 }
